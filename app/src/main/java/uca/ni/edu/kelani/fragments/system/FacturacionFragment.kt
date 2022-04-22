c1326637da7a9d4c5c2f2e799bbd715573f106de
MarlieR-321 <marlie.ramirez12169@est.uca.edu.ni>
2022-04-21 20:21:25 -0600

    Facturacion

--- app/src/main/java/uca/ni/edu/kelani/fragments/system/FacturacionFragment.kt
+++ app/src/main/java/uca/ni/edu/kelani/fragments/system/FacturacionFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import uca.ni.edu.kelani.R
 import uca.ni.edu.kelani.databinding.FragmentFacturacionBinding
 import uca.ni.edu.kelani.databinding.FragmentInicioSecionBinding
 
@@ -19,6 +21,14 @@ class FacturacionFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentFacturacionBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAdd.setOnClickListener {
+            findNavController().navigate(R.id.frmFacturaFragment)
+        }
     }
 
 
---
+++ app/src/main/java/uca/ni/edu/kelani/fragments/system/FrmFacturaFragment.kt
@@ -0,0 +1,62 @@
+package uca.ni.edu.kelani.fragments.system
+
+import android.app.DatePickerDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import uca.ni.edu.kelani.R
+import uca.ni.edu.kelani.databinding.FragmentFacturacionBinding
+import uca.ni.edu.kelani.databinding.FragmentFrmFacturaBinding
+import java.util.*
+
+
+class FrmFacturaFragment : Fragment() {
+    private lateinit var binding: FragmentFrmFacturaBinding
+    private lateinit var mDateSetListener: DatePickerDialog.OnDateSetListener
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentFrmFacturaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        datePicker()
+
+        binding.btnSave.setOnClickListener {
+            findNavController().navigate(R.id.frmFacturaDetFragment)
+        }
+    }
+
+    private fun datePicker(){
+        binding.itFecha.setOnClickListener {
+            var cal: Calendar = Calendar.getInstance()
+            var year = cal.get(Calendar.YEAR)
+            var month = cal.get(Calendar.MONTH)
+            var day = cal.get(Calendar.DAY_OF_MONTH)
+
+            var dialog = DatePickerDialog(
+                requireContext(), android.R.style.Theme_Holo_Dialog_MinWidth,
+                mDateSetListener, year, month,day
+            )
+
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.show()
+        }
+
+        mDateSetListener = DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+            binding.itFecha.setText("$day / ${month+1} / $year")
+        }
+
+    }
+}
