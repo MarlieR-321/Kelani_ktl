058a349d624b2e722a51679acd3721587729a39a
Cath1999 <catherine.mena11751@est.uca.edu.ni>
2022-04-23 17:58:29 -0600

    UnidadMedida

--- app/src/main/java/uca/ni/edu/kelani/fragments/system/AgregarUnidadDeMedidaFragment.kt
+++ app/src/main/java/uca/ni/edu/kelani/fragments/system/AgregarUnidadDeMedidaFragment.kt
@@ -7,32 +7,31 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import uca.ni.edu.kelani.R
-import uca.ni.edu.kelani.databinding.FragmentCategoriaBinding
-import uca.ni.edu.kelani.databinding.FragmentFacturacionBinding
-import uca.ni.edu.kelani.databinding.FragmentUsuariosBinding
+import uca.ni.edu.kelani.databinding.FragmentAgregarClientesBinding
+import uca.ni.edu.kelani.databinding.FragmentAgregarUnidadBinding
 
-
-class CategoriaFragment : Fragment() {
-    private lateinit var _binding: FragmentCategoriaBinding
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+class AgregarUnidadDeMedidaFragment : Fragment() {
+    private  lateinit var  binding: FragmentAgregarUnidadBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentCategoriaBinding.inflate(inflater, container, false)
+        binding = FragmentAgregarUnidadBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnAdd.setOnClickListener {
-            findNavController().navigate(R.id.action_categoriaFragment2_to_agregarCategoriaFragment)
+        binding.btnGuardarMedida.setOnClickListener {
+            findNavController().navigate(R.id.action_agregarClientesFragment_to_nav_clients)
         }
     }
+
+    /*override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }*/
 }
