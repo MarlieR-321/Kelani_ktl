ab905079e67664bdb715a7246657f7d011c616a3
MarlieR-321 <marlie.ramirez12169@est.uca.edu.ni>
2022-04-24 17:25:14 -0600

    Nuevo Diseño de Facturación

--- app/src/main/java/uca/ni/edu/kelani/fragments/system/FacturacionDetFragment.kt
+++ app/src/main/java/uca/ni/edu/kelani/fragments/system/FacturacionDetFragment.kt
@@ -5,20 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import uca.ni.edu.kelani.R
+import uca.ni.edu.kelani.databinding.FragmentFactutacionDetBinding
 
 // TODO: Rename parameter arguments, choose names that match
 
 class FacturacionDetFragment : Fragment() {
     // TODO: Rename and change types of parameters
 
+    private lateinit var binding: FragmentFactutacionDetBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_factutacion_det, container, false)
+        binding = FragmentFactutacionDetBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAdd.setOnClickListener {
+            findNavController().navigate(R.id.frmFacturaDetFragment)
+        }
+    }
 }
