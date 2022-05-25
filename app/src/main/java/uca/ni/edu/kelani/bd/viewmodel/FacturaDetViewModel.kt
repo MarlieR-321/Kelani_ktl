e73f0255e92897c983f4b58181012acacb2df51f
MarlieR-321 <marlie.ramirez12169@est.uca.edu.ni>
2022-05-24 21:38:45 -0600

    refresh agregar detalle

--- app/src/main/java/uca/ni/edu/kelani/bd/viewmodel/FacturaDetViewModel.kt
+++ app/src/main/java/uca/ni/edu/kelani/bd/viewmodel/FacturaDetViewModel.kt
@@ -2,15 +2,11 @@ package uca.ni.edu.kelani.bd.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import uca.ni.edu.kelani.bd.dao.bdKealni
-import uca.ni.edu.kelani.bd.dao.FacturaDetDao
 import uca.ni.edu.kelani.bd.entidades.FacturaDet
-import uca.ni.edu.kelani.bd.entidades.views.vw_Factura
 import uca.ni.edu.kelani.bd.entidades.views.vw_FacturaDet
 import uca.ni.edu.kelani.bd.repository.FacturaDetRepository
 
@@ -34,15 +30,14 @@ class FacturaDetViewModel(application: Application): AndroidViewModel(applicatio
     fun agregarFacturaDet(f: FacturaDet) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.add(f)
-            fetchFactura()
         }
+        fetchFactura()
     }
 
 
     fun eliminarFacturaDet(f: FacturaDet) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.delete(f)
-            //fetchFactura()
         }
     }
 }
